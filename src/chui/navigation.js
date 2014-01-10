6f8d16179585468861e7efa96b9e130f0627ba70
sandwichfeet <sandwichfeet@gmail.com>
2014-01-10 21:50:19 +0000

    Fix $.UIGoToArticle $.UINavigationHistory update
    UIGoToArticle wasn't setting the correct destination id in UINavigationHistory.  Added the '#' to stop the back button from breaking.

--- src/chui/navigation.js
+++ src/chui/navigation.js
@@ -137,7 +137,7 @@
       var currentToolbar;
       var destinationToolbar;
       $.publish('chui/navigate/leave', current[0].id);
-      $.UINavigationHistory.push(destinationID);
+      $.UINavigationHistory.push('#' + destinationID);
       $.publish('chui/navigate/enter', destination[0].id);
       current[0].scrollTop = 0;
       destination[0].scrollTop = 0;
